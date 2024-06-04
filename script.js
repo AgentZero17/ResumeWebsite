f480a31a25ea706ece4ebbb846af2483e18f533f
Enea Meta <48912650+AgentZero17@users.noreply.github.com>
2024-06-03 23:11:48 -0500

    Add files via upload
    website resume

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.getElementById("header");
+    const content = document.getElementById("content");
+    const elements = [header, content];
+    const typeSpeed = 50;
+    let currentElement = 0;
+    let currentChar =
